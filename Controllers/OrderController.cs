121d083a5fe779242cd798f735432a88089335e0
Kitajima2910 <kitajima2910@gmail.com>
2020-12-22 11:30:44 +0700

    fix OrderController.cs and UserController.cs

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -10,20 +10,22 @@ namespace DemoProject.Controllers
 {
     public class OrderController : Controller
     {
-        private IOrderServices services;
+        private readonly IOrderServices _services;
 
         public OrderController(IOrderServices services)
         {
-            this.services = services;
+            this._services = services;
         }
         public IActionResult Index()
         {
+            // ReSharper disable once Mvc.ViewNotResolved
             return View();
         }
 
         public IActionResult UpdateStatus(int id, int userid)
         {
-            services.UpdateStatus(id);
+            _services.UpdateStatus(id);
+            // ReSharper disable once Mvc.ActionNotResolved
             return RedirectToAction("Order", new RouteValueDictionary(
             new { controller = "User", action = "Order", Id = userid }));
         }
